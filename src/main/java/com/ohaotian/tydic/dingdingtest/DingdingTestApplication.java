d829496286a5b7e9b7f769adb2369560b0b1424a
lzh <279023678@qq.com>
2019-02-18 09:27:32 +0800

    测试钉钉

---
+++ src/main/java/com/ohaotian/tydic/dingdingtest/DingdingTestApplication.java
@@ -0,0 +1,13 @@
+package com.ohaotian.tydic.dingdingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingdingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DingdingTestApplication.class, args);
+    }
+
+}
